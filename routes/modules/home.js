708ae53870c029ea02d879fa78d025dc0dff1c1f
YangYGYang <ginayoung82@gmail.com>
2022-08-20 19:54:52 +0800

    add index connect db data

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const ACCschema = require('../../models/accounting')
 
 router.get('/', (req, res) => {
-    res.render('index')
+    ACCschema.find()
+        .lean()
+        .then((accData) => {
+            res.render('index', { accData })
+        })
+        .catch(error => console.log(error))
+
 })
 
 module.exports = router
