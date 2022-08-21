12da757796e360e4f861e2e4c1e6ef16d2b92bf9
YangYGYang <ginayoung82@gmail.com>
2022-08-21 14:39:21 +0800

    add register router

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-    // const ACCschema = require('../../models/accounting')
+const USERschema = require('../../models/user')
 
 router.get('/login', (req, res) => {
     res.render('login')
@@ -8,6 +8,22 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const registerData = req.body
+    console.log(registerData)
+    USERschema.findOne({ email: registerData.email })
+        .then((user) => {
+            if (!user) {
+                USERschema.create(registerData)
+                    .then(() => res.redirect('/'))
+                    .catch(error => consoel.log(error))
+            } else {
+                res.redirect('login')
+            }
+        })
+        .catch(error => consoel.log(error))
 })
 
 module.exports = router
