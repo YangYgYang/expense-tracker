fba6e359f92c1f77a95274440ca9c8d907d10f49
YangYGYang <ginayoung82@gmail.com>
2022-09-03 11:25:10 +0800

    add subtotals

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,7 +8,12 @@ router.get('/', (req, res) => {
     ACCschema.find({ userId: userid })
         .lean()
         .then((accData) => {
-            res.render('index', { accData })
+            let subTotals = 0
+            accData.forEach(acc => {
+                subTotals += acc.subTotal
+            })
+            console.log(subTotals)
+            res.render('index', { accData, subTotals })
         })
         .catch(error => console.log(error))
 
