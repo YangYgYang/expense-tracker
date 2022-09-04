dc6800133f82381a1a451b5582de4842ce02b970
YangYGYang <ginayoung82@gmail.com>
2022-09-04 13:05:44 +0800

    add username in main layout

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,22 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const ACCschema = require('../../models/accounting')
+const Userschema = require('../../models/user')
 
 router.get('/', (req, res) => {
     const userid = req.user.user_id
     const getAccounting = req.query
     let accDataFin = []
+    let username = ''
+    Userschema.findOne({ _id: userid })
+        .lean()
+        .then((user) => {
+            username = user.name
+        })
         // console.log('進入首頁後', userid)
     ACCschema.find({ userId: userid })
         .lean()
         .then((accData) => {
-            if (getAccounting) {
-                if (getAccounting.categoryId !== '0') {
-                    console.log('進到修改路由', getAccounting)
-                    accDataFin = accData.filter(e => e.categoryId === getAccounting.categoryId)
-                } else {
-                    accDataFin = accData
-                }
+            console.log('到底有沒有東西', getAccounting)
+            if (getAccounting.categoryId == 'none' || getAccounting.categoryId === undefined) {
+                console.log('有進到這邊嗎')
+                accDataFin = accData
+            } else {
+                accDataFin = accData.filter(e => e.categoryId === getAccounting.categoryId)
             }
             let subTotals = 0
                 // console.log(accData)
@@ -28,7 +34,7 @@ router.get('/', (req, res) => {
                     day: acc.date.getDate()
                 }
             })
-            res.render('index', { accDataFin, subTotals, userid })
+            res.render('index', { accDataFin, subTotals, userid, accData, username })
         })
         .catch(error => console.log(error))
 })
