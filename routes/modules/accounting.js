8360c7c9ae4a6c9d70208d559bdd422f23b16a9a
YangYgYang <you@example.com>
2022-11-07 23:31:08 +0800

    update style

--- routes/modules/accounting.js
+++ routes/modules/accounting.js
@@ -18,13 +18,11 @@ router.post('/create', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-    let accountingID = req.body._id
-    ACCschema.findOne({ accountingID })
+    let accountingID = req.params.id
+    ACCschema.findOne({ _id: accountingID  })
         .then((accounting) => {
             //這邊使用同步的點，是因為必須有先後順序，不然會一直還沒刪除，就redirect
             accounting.remove({}, (err, docs) => {
-                console.log('err', err)
-                console.log('docs', docs)
                 res.redirect('/')
             })
 
